58b5ce94f51fab6e5e54b44993af5f06273e2ec0
Víctor Muñoz Ramírez <vicmunram@alum.us.es>
2023-01-28 17:39:22 +0100

    More refactoring

---
+++ src/scripts/items/AmmoPack.js
@@ -0,0 +1,15 @@
+export default class AmmoPack extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y) {
+        super(scene, x + 8, y + 12.5, 'ammoPack');
+        this.scene = scene;
+        this.scene.add.existing(this);
+        this.scene.physics.add.existing(this);
+        this.body.setAllowGravity(false);
+    }
+
+    use() {
+        this.scene.sound.play('pickUp');
+        this.disableBody(true, true);
+        this.scene.player.refillAmmo();
+    }
+}
